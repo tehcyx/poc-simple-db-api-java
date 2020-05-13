6dd12a1db34f36dbe8f460dc256f624b6bc6e5cf
Daniel Roth <daniel.roth02@sap.com>
2020-05-13 14:33:03 -0700

    disable ssl in cluster

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ set -ex
 # bash array
 ARGS=()
 
-DB_URL="jdbc:postgresql://${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
+DB_URL="jdbc:postgresql://${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?useSSL=false"
 
 ARGS+=(-Dspring.datasource.url="${DB_URL}" -Dspring.datasource.username="${POSTGRES_USER}" -Dspring.datasource.password="${POSTGRES_PASSWORD}")
 
